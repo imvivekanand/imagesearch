5087cab6ac7756fa252d7f00bba3e2cd718f82a1
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2022-12-06 00:31:33 +0530

    Api Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term){
-    console.log(term)
+  state = {images: []}; //If the output is an array the set the state as the empty array.
+
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID 2tYYF1ggmpGjEUBjgM5ABCIO0JlZ_WRekxkE0oe9tlw",
+      },
+    });
+
+    this.setState({images: response.data.results});
   }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.imagess.length} images
       </div>
     );
   }
